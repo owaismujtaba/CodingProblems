71cbb0e4c04d4971e6b5b8c324477790d8f8c4ec
owaismujtaba <owais.mujtaba123@gmail.com>
2023-10-31 00:44:27 +0100

    search insertion pos

--- merge_two_sorted_arrays.py
+++ merge_two_sorted_arrays.py
@@ -12,28 +12,68 @@ class ListNode:
         self.val = val
         self.next = next
     
-    def merge_two_lists
+def merge_two_lists(l1, l2):
+        
+    head = ListNode(0)
+    temp = head
+    while l1 or l2:
+        if l1 and l2:
+            
+            if l1.val <= l2.val:
+                temp.next = ListNode(l1.val)
+                print('l1',l1.val)
+                l1 = l1.next
+            else:
+                temp.next = ListNode(l2.val)
+                print('l2',l2.val)
+                l2 = l2.next
+            
+            temp = temp.next
+            print(temp.val)    
+        elif l1:
+            temp.next = l1
+            return head
+        else:
+            temp.next = l2
+            return head
+                
+        return head.next
 
         
         
-def create_list(numbers):
-    numbers =np.random.randint(0, 10, size=10)
+def create_list(n):
+    numbers =np.random.randint(0, 10, size=n)
     head = ListNode(numbers[0])
     temp = head
-    for i in range(1, n):
+    for i in range(n):
         temp.next = ListNode(numbers[i])
         temp = temp.next
         
     return head
 def show_list(head):
     temp = head
+    items = []
     while temp:
-        print(temp.val)
+        items.append(temp.val)
         temp = temp.next
-        
-        
+    print(items)
+    
+def create_list_from_list(items):
+    head = ListNode(0)
+    temp = head
+    for item in items:
+        temp.next = ListNode(item)
+        temp = temp.next
+    return head.next
+
 if __name__=='__main__':
-    l1 = create_list(10)
-    l2 = create_list(5)
+    a = [1,3,5,7]
+    b = [2,2,4,5,6,9]
+    l1 = create_list_from_list(a)
+    l2 = create_list_from_list(b)
     
     show_list(l1)
+    show_list(l2)
+    l3 = merge_two_lists(l1,l2)
+    show_list(l3)
+    print(a[-1])
---
+++ search_insert_position.py
@@ -0,0 +1,26 @@
+'''
+Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+'''
+class SearchInsertPosition:
+    
+    def search_insert_position(self, arr, target):
+        left, right = 0, len(arr)
+        print(left, right)
+        while left <= right:
+            middle = (left + right)//2
+            if arr[middle] == target:
+                return middle
+            #elif right - left == 1:
+            #    return left
+            elif arr[middle] < target:
+                left = middle
+            else:
+                right = middle
+            
+if __name__=='__main__':
+    obj = SearchInsertPosition()
+    a = [[1,3,5,6]]
+    print(obj.search_insert_position(a, target=2))
